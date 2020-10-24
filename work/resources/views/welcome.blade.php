220ec1efd753fa7a66c02c4c51b03fd052b8e1e6
sait0-kun <kitsit.39@gmail.com>
2020-10-24 09:24:40 +0900

    アプリ名の追加

--- work/resources/views/welcome.blade.php
+++ work/resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>{{ env('APP_NAME', 'Laravel') }}</title>
+
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
