92ef0d6d9ca188883581e2d19f269f2c5621ea6b
sait0-kun <kitsit.39@gmail.com>
2020-11-01 17:23:14 +0900

    dragBoardItemで表示する内容をcontextに切り分けた

--- work/resources/ts/contexts/TotalContext.tsx
+++ work/resources/ts/contexts/TotalContext.tsx
@@ -1,12 +1,15 @@
 import React, { createContext } from 'react';
 import { StylesContextProvider } from './childContexts/StylesContext';
+import { BoardItemContextProvider } from './childContexts/BoardItemContext';
 
 export const TotalContext = createContext({});
 
 export const TotalContextProvider: React.FC = props => {
   return (
     <StylesContextProvider>
-      {props.children}
+      <BoardItemContextProvider>
+        {props.children}
+      </BoardItemContextProvider>
     </StylesContextProvider>
   )
 }
