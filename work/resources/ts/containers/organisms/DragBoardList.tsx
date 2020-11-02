f82c6ec54daa99a5c1228b77e86dda39d0ffb7a5
sait0-kun <kitsit.39@gmail.com>
2020-11-02 20:40:24 +0900

    更新後、モーダルクローズする

--- work/resources/ts/containers/organisms/DragBoardList.tsx
+++ work/resources/ts/containers/organisms/DragBoardList.tsx
@@ -91,6 +91,7 @@ export const DragBoardList = React.memo<BoardListProps> (({
           setBoardItemState(
             newBoardItemState,
           );
+          modalClose();
         }}
       />
     </>
