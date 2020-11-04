63c343b580f2bdf5cd1e2a5c7e94f9502931f3e4
sait0-kun <kitsit.39@gmail.com>
2020-11-04 21:28:00 +0900

    rename

--- work/resources/ts/containers/organisms/Preview.tsx
+++ work/resources/ts/containers/organisms/Preview.tsx
@@ -2,14 +2,27 @@ import React, { useState, useContext } from 'react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import { StylesContext } from '../../contexts/childContexts/StylesContext';
 
+type props = {
+  items: {
+    id: string,
+    content: string,
+  },
+}
+
 const source = `
 ## MarkdownPreview
 
 > todo: React component preview markdown text.
 `;
 
-export const Demo: React.FC<any> = () => {
+export const Preview: React.FC<props> = ({
+  items
+}) => {
+  const item = JSON.stringify(items,null,'\t');
+
   return (
-    <MarkdownPreview source={source} />
+    <MarkdownPreview
+      source={item}
+    />
   );
 }
