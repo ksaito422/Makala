1aa82176cb30a29dd16f525334df53f8a3d23b3b
sait0-kun <kitsit.39@gmail.com>
2020-11-08 13:57:14 +0900

    app.tsxのリファクタ themeを別ファイルに移動

--- work/resources/ts/app.tsx
+++ work/resources/ts/app.tsx
@@ -1,52 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import {
-  createMuiTheme,
-  MuiThemeProvider,
-} from '@material-ui/core';
-import { blue, deepPurple } from '@material-ui/core/colors';
 import { TotalContextProvider } from './contexts/TotalContext';
 import { HomePage } from './containers/pages/HomePage';
-
-// ページ全体で使うテーマカラーの定義
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      light: deepPurple[300],
-      main: deepPurple[500],
-      dark: deepPurple[700],
-    },
-    secondary: {
-      light: blue[300],
-      main: blue[500],
-      dark: blue[700],
-    },
-  },
-  overrides: {
-    MuiButton: {
-      root: {
-        textTransform: `none`,
-      },
-      label: {
-        justifyContent: `none`,
-      }
-    }
-  }
-});
+import { MuiTheme } from './theme/MuiTheme';
+import { MuiThemeProvider } from '@material-ui/core';
 
 const App: React.FC = () => {
-    return (
-      <Router>
-        <TotalContextProvider>
-          <Switch>
-            <MuiThemeProvider theme={theme}>
-              <Route exact path='/' component={HomePage}></Route>
-            </MuiThemeProvider>
-          </Switch>
-        </TotalContextProvider>
-      </Router>
-    )
+  return (
+    <Router>
+      <TotalContextProvider>
+        <Switch>
+          <MuiThemeProvider theme={MuiTheme}>
+            <Route exact path='/' component={HomePage}></Route>
+          </MuiThemeProvider>
+        </Switch>
+      </TotalContextProvider>
+    </Router>
+  );
 }
 
 if (document.getElementById('app')) {
