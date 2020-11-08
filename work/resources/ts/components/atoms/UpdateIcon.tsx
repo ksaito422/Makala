b61b484bb7827af4b85fb44a647cb5dfec7b30a5
sait0-kun <kitsit.39@gmail.com>
2020-11-08 11:50:47 +0900

    UpdateIcon.tsxをリファクタ

--- work/resources/ts/components/atoms/UpdateIcon.tsx
+++ work/resources/ts/components/atoms/UpdateIcon.tsx
@@ -3,18 +3,17 @@ import { IconButton } from '@material-ui/core';
 import { SystemUpdateAlt } from '@material-ui/icons';
 
 type Props = {
-  onClick: (event: any) => void,
   disabled?: boolean,
+  onClick: () => void,
 }
 
 export const UpdateIcon: React.FC<Props> = (props: Props) => {
   return (
     <IconButton
-      component='span'
       onClick={props.onClick}
       disabled={props.disabled}
     >
       <SystemUpdateAlt />
     </IconButton>
-  )
+  );
 }
