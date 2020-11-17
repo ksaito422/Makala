00ce2e067528a9d6851596fc06d09aa299a9aeaf
sait0-kun <kitsit.39@gmail.com>
2020-11-17 20:09:33 +0900

    テストユーザーアカウントの作成

--- work/database/seeds/UsersTableSeeder.php
+++ work/database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,6 +12,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'test-user',
+                'email' => 'test-user@example.com',
+                'password' => \Hash::make('test1234')
+            ],
+            [
+                'name' => 'saito',
+                'email' => 'saito@example.com',
+                'password' => \Hash::make('saito1234')
+            ]
+        ]);
     }
 }
