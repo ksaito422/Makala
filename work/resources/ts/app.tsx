ca685787f0caff33f6a6c2c16752a5dc3ff9aa8c
sait0-kun <kitsit.39@gmail.com>
2020-11-21 11:33:31 +0900

    ログインページへのルーティングを追加

--- work/resources/ts/app.tsx
+++ work/resources/ts/app.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { TotalContextProvider } from './contexts/TotalContext';
 import { Top } from './containers/pages/Top';
+import { Login } from './containers/pages/Login';
 import { HomePage } from './containers/pages/HomePage';
 import { MuiTheme } from './theme/MuiTheme';
 import { MuiThemeProvider } from '@material-ui/core';
@@ -14,6 +15,7 @@ const App: React.FC = () => {
         <Switch>
           <MuiThemeProvider theme={MuiTheme}>
             <Route exact path='/' component={Top}></Route>
+            <Route exact path='/login' component={Login}></Route>
             <Route exact path='/home' component={HomePage}></Route>
           </MuiThemeProvider>
         </Switch>
