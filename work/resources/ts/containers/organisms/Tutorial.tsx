f2c6fb1c5e9338ed8a4c75080559da96399c3026
sait0-kun <kitsit.39@gmail.com>
2020-11-21 09:29:55 +0900

    非ログインユーザー用のチュートリアルコンポーネントを追加

---
+++ work/resources/ts/containers/organisms/Tutorial.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * Please import if necessary
+*/
+// import { StylesContext } from '../../contexts/childContexts/StylesContext';
+// import {  } from '@material-ui/core';
+// import {  } from '@material-ui/icons';
+
+type Props = {
+  // Defines the type of props.
+
+}
+
+export const Tutorial: React.FC<Props> = (props: Props) => {
+  return (
+    <>
+      <p>トップ画面です</p>
+
+    </>
+  );
+}
