25f46d9a4ff94029c56f682777a63fe3d3141086
sait0-kun <kitsit.39@gmail.com>
2020-11-22 17:13:10 +0900

    [save]ユーザー登録の確認画面を追加

---
+++ work/resources/ts/containers/organisms/Confirm.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/**
+ * Please import if necessary
+*/
+// import { StylesContext } from '../../contexts/childContexts/StylesContext';
+// import {  } from '@material-ui/core';
+// import {  } from '@material-ui/icons';
+
+type Props = {
+  // Defines the type of props.
+
+}
+
+export const Confirm: React.FC<Props> = (props: Props) => {
+  return (
+    <>
+      <p>確認画面</p>
+
+    </>
+  );
+}
