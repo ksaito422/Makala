17830809d5539ee934691f27c8e9dd33b1d6da9e
sait0-kun <kitsit.39@gmail.com>
2020-11-22 10:57:23 +0900

    ボード名の更新機能を実装

--- work/resources/ts/containers/organisms/Boards.tsx
+++ work/resources/ts/containers/organisms/Boards.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CloseIcon } from '../../components/atoms/CloseIcon';
+import { ModalWindow } from '../../components/molecules/ModalWindow';
 import { StylesContext } from '../../contexts/childContexts/StylesContext';
 import {
   Container,
@@ -11,15 +12,32 @@ import {
   ListItemSecondaryAction,
   Paper,
  } from '@material-ui/core';
- import DragIndicatorIcon from '@material-ui/icons/DragIndicator';
+import CreateIcon from '@material-ui/icons/Create';
 
 type Props = {
   boards: any,
-  onClick: () => void,
+  // onClick: () => void,
+  postOnClick: (data: any) => void,
   deleteOnClick: (event: number) => void,
 }
 
 export const Boards: React.FC<Props> = (props: Props) => {
+  // モーダルに渡す表示内容
+  const [modalValueState, setmodalValueState] = useState<any>({
+    id: null,
+    board_name: null
+  });
+  // モーダル表示のon/off切り替え
+  const [modalOpenState, setModalOpenState] = useState<boolean>(false);
+  // モーダルを閉じるとき、入力値をクリア
+  const modalClose = () => {
+    setModalOpenState(false);
+    setmodalValueState({
+      id: null,
+      board_name: null
+    });
+  };
+
   return (
     <>
       <Container maxWidth='sm'>
@@ -35,9 +53,16 @@ export const Boards: React.FC<Props> = (props: Props) => {
               <ListItem key={index}>
                 <ListItemIcon>
                   <IconButton
-                    onClick={props.onClick}
+                    onClick={() => {
+                      setModalOpenState(true);
+                      setmodalValueState({
+                        ...modalValueState,
+                        id: board.id,
+                        board_name: board.board_name,
+                      })
+                    }}
                   >
-                    <DragIndicatorIcon />
+                    <CreateIcon />
                   </IconButton>
                 </ListItemIcon>
                 <ListItemText
@@ -54,6 +79,23 @@ export const Boards: React.FC<Props> = (props: Props) => {
               ))}
           </List>
         </Paper>
+        <ModalWindow
+          modalOpen={modalOpenState}
+          modalOnClose={modalClose}
+          postOnClick={() => {
+            props.postOnClick(modalValueState)
+          }}
+          defaultValueTitle={modalValueState.board_name}
+          // いらない
+          defaultValueContent={''}
+          titleOnChange={(e) => {
+            setmodalValueState({ ...modalValueState, board_name: e.target.value });
+          }}
+          // いらない
+          contentOnChange={() => {
+            console.log('hello')
+          }}
+        />
       </Container>
     </>
   );
