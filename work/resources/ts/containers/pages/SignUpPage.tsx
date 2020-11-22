d451a06e2ce7ad3d47afb8b65ee5182ff5c856ce
sait0-kun <kitsit.39@gmail.com>
2020-11-22 18:27:00 +0900

    onChangeで認証情報を追加していく

--- work/resources/ts/containers/pages/SignUpPage.tsx
+++ work/resources/ts/containers/pages/SignUpPage.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Header } from '../organisms/Header';
 import { SignUp } from '../organisms/SignUp';
+import { AuthContext } from '../../contexts/childContexts/AuthContext';
 import { StylesContext } from '../../contexts/childContexts/StylesContext';
 import {
   Container,
@@ -10,6 +11,7 @@ import {
 } from '@material-ui/core';
 
 export const SignUpPage: React.FC = () => {
+  const { authState, setAuthState } = useContext<any>(AuthContext);
   const { useStyles } = useContext<any>(StylesContext);
   const classes = useStyles();
 
@@ -20,7 +22,21 @@ export const SignUpPage: React.FC = () => {
         title='makala'
       />
       <Container maxWidth='xl' className={classes.main_container}>
-        <SignUp />
+        <SignUp
+          // 認証情報をstateに保持していく
+          nameOnChange={(e) => {
+            setAuthState({ ...authState, name: e.target.value });
+          }}
+          mailOnChange={(e) => {
+            setAuthState({ ...authState, email: e.target.value });
+          }}
+          passwordOnChange={(e) => {
+            setAuthState({ ...authState, password: e.target.value });
+          }}
+          passConfirmOnChange={(e) => {
+            setAuthState({ ...authState, pass_confirm: e.target.value });
+          }}
+        />
       </Container>
     </>
   )
