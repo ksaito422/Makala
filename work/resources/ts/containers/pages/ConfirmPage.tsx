6c521ce7aa7376e98fa9c26127161aed6f037a3b
sait0-kun <kitsit.39@gmail.com>
2020-11-22 18:37:30 +0900

    認証情報をクッション画面に渡す

--- work/resources/ts/containers/pages/ConfirmPage.tsx
+++ work/resources/ts/containers/pages/ConfirmPage.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Header } from '../organisms/Header';
 import { Confirm } from '../organisms/Confirm';
+import { AuthContext } from '../../contexts/childContexts/AuthContext';
 import { StylesContext } from '../../contexts/childContexts/StylesContext';
 import {
   Container,
@@ -10,6 +11,7 @@ import {
 } from '@material-ui/core';
 
 export const ConfirmPage: React.FC = () => {
+  const { authState } = useContext<any>(AuthContext);
   const { useStyles } = useContext<any>(StylesContext);
   const classes = useStyles();
 
@@ -20,7 +22,13 @@ export const ConfirmPage: React.FC = () => {
         title='makala'
       />
       <Container maxWidth='xl' className={classes.main_container}>
-        <Confirm />
+        <Confirm
+          // 認証情報の再確認のため表示するデータを渡す
+          name={authState.name}
+          email={authState.email}
+          password={authState.password}
+          pass_confirm={authState.pass_confirm}
+        />
       </Container>
     </>
   )
