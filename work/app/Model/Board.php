a0e8d64e5472259a93021366e384ce1df11f32f7
sait0-kun <kitsit.39@gmail.com>
2020-11-26 19:26:52 +0900

    Board -> Card の一対多リレーションを定義

--- work/app/Model/Board.php
+++ work/app/Model/Board.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Card;
 
 class Board extends Model
 {
-    //
+    public function cards()
+    {
+        return $this->hasMany(Card::class);
+    }
 }
