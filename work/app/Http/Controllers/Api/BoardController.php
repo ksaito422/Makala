c474266e5e79f38550661a15c3463eaf7542340a
sait0-kun <kitsit.39@gmail.com>
2020-11-28 16:58:36 +0900

    BoardControllerのメソッドを認証ガードした

--- work/app/Http/Controllers/Api/BoardController.php
+++ work/app/Http/Controllers/Api/BoardController.php
@@ -10,6 +10,14 @@ use App\Model\Board;
 class BoardController extends Controller
 {
     /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
