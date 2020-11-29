9695ad9c41746ffe8e11644e6694589229401399
sait0-kun <kitsit.39@gmail.com>
2020-11-30 06:25:34 +0900

    スピナーと背景のmoleculeを作成

---
+++ work/resources/ts/components/molecules/Spinner.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import { StylesContext } from '../../contexts/childContexts/StylesContext';
+import {
+  Backdrop,
+  CircularProgress,
+} from '@material-ui/core';
+
+type Props = {
+  open: boolean,
+}
+
+export const Spinner: React.FC<Props> = (props) => {
+  const { useStyles } = useContext<any>(StylesContext);
+  const classes = useStyles();
+
+  return (
+    <>
+      <Backdrop open={props.open} className={classes.Spinner}>
+        <CircularProgress color='secondary' />
+      </Backdrop>
+    </>
+  );
+}
