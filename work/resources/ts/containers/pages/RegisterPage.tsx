f6fa6a19cc1c1f4736499d1550a1d5afde03dfaa
sait0-kun <kitsit.39@gmail.com>
2020-12-06 17:40:52 +0900

    RegisterPage.tsxにスピナーと通知の追加

--- work/resources/ts/containers/pages/RegisterPage.tsx
+++ work/resources/ts/containers/pages/RegisterPage.tsx
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Header } from '../organisms/Header';
 import { Register } from '../organisms/Register';
+import { Spinner } from '../../components/molecules/Spinner';
+import { Notice } from '../../components/molecules/Notice';
+import { FeedbackContext } from '../../contexts/childContexts/FeedbackContext';
 import { StylesContext } from '../../contexts/childContexts/StylesContext';
 import {
   Container,
@@ -9,9 +12,11 @@ import {
 } from '@material-ui/core';
 
 export const RegisterPage: React.FC = () => {
-  /** cssの定義
+  /** api通信中のスピナー表示のon/off管理
+   * cssの定義
    * react-router-dom URLルーティングに使う
    */
+  const { progress, status, setStatus } = useContext<any>(FeedbackContext);
   const { useStyles } = useContext<any>(StylesContext);
   const classes = useStyles();
   const history = useHistory();
@@ -30,6 +35,16 @@ export const RegisterPage: React.FC = () => {
           }}
         />
       </Container>
+
+      <Spinner open={progress} />
+      <Notice
+        open={status.open}
+        type={status.type}
+        message={status.message}
+        onClose={() => {
+          setStatus({ ...status, open: false });
+        }}
+      />
     </>
   );
 }
