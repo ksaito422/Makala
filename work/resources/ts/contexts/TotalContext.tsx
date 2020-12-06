5d64323764010465fa53fb4780f3358f74b585fd
sait0-kun <kitsit.39@gmail.com>
2020-12-06 10:30:52 +0900

    ModalPropsContextの登録

--- work/resources/ts/contexts/TotalContext.tsx
+++ work/resources/ts/contexts/TotalContext.tsx
@@ -4,6 +4,7 @@ import { FeedbackContextProvider } from './childContexts/FeedbackContext';
 import { AuthContextProvider } from './childContexts/AuthContext';
 import { ApiBoardsContextProvider } from './childContexts/ApiBoardsContext';
 import { ApiCardsContextProvider } from './childContexts/ApiCardsContext';
+import { ModalPropsContextProvider } from './childContexts/ModalPropsContext';
 import { BoardItemContextProvider } from './childContexts/BoardItemContext';
 
 export const TotalContext = createContext({});
@@ -15,9 +16,11 @@ export const TotalContextProvider: React.FC = props => {
         <AuthContextProvider>
           <ApiBoardsContextProvider>
             <ApiCardsContextProvider>
-              <BoardItemContextProvider>
-                {props.children}
-              </BoardItemContextProvider>
+              <ModalPropsContextProvider>
+                <BoardItemContextProvider>
+                  {props.children}
+                </BoardItemContextProvider>
+              </ModalPropsContextProvider>
             </ApiCardsContextProvider>
           </ApiBoardsContextProvider>
         </AuthContextProvider>
