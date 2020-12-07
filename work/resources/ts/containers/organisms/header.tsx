b9fc4752e62be970548fc9c180595f99f6d870a7
sait0-kun <kitsit.39@gmail.com>
2020-12-07 09:55:47 +0900

    headerにアバターを表示

--- work/resources/ts/containers/organisms/header.tsx
+++ work/resources/ts/containers/organisms/header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Title } from '../../components/atoms/Title';
+import { Avatar } from '../../components/atoms/Avatar';
 import {
   AppBar,
+  Grid,
   Toolbar,
 } from '@material-ui/core';
 
@@ -9,9 +11,20 @@ export const Header: React.FC = () => {
   return (
     <AppBar color='primary' position='sticky'>
       <Toolbar>
-        <Title
-          title='makala'
-        />
+        <Grid container spacing={2}>
+          <Grid item xs={11}>
+            <Title title='makala' />
+          </Grid>
+          <Grid item xs={1}>
+            <Avatar
+              onClick={() => {
+                console.log('avatar!!');
+              }}
+            >
+              A
+            </Avatar>
+          </Grid>
+        </Grid>
       </Toolbar>
     </AppBar>
   );
