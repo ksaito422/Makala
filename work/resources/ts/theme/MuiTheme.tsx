39ffb39c90479bdf59eb43701feb8844c1ff197d
sait0-kun <kitsit.39@gmail.com>
2020-12-09 08:25:45 +0900

    [こだわり]カラーテーマの修正

--- work/resources/ts/theme/MuiTheme.tsx
+++ work/resources/ts/theme/MuiTheme.tsx
@@ -1,33 +1,25 @@
 import { createMuiTheme } from '@material-ui/core';
-import { blue, deepPurple, yellow } from '@material-ui/core/colors';
+import { deepPurple, yellow } from '@material-ui/core/colors';
 
 // ページ全体で使うテーマカラーの定義
 export const MuiTheme = createMuiTheme({
   palette: {
     primary: {
-      light: deepPurple[300],
-      main: deepPurple[500],
-      dark: deepPurple[700],
-    },
-    secondary: {
       light: yellow[100],
       main: yellow[500],
       dark: yellow[900],
     },
-    // secondary: {
-    //   light: blue[300],
-    //   main: blue[500],
-    //   dark: blue[700],
-    // },
+    secondary: {
+      light: deepPurple[300],
+      main: deepPurple[500],
+      dark: deepPurple[700],
+    },
   },
   overrides: {
     MuiButton: {
       root: {
         textTransform: `none`,
       },
-      label: {
-        // justifyContent: `none`,
-      }
     },
     MuiListItemText: {
       root: {
