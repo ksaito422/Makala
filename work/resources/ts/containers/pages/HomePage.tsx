93e0a95401494ff89c84a28ae4625146432f25c5
sait0-kun <kitsit.39@gmail.com>
2020-12-09 11:49:24 +0900

    [readability]コードの整形

--- work/resources/ts/containers/pages/HomePage.tsx
+++ work/resources/ts/containers/pages/HomePage.tsx
@@ -13,10 +13,24 @@ import {
 } from '@material-ui/core';
 
 export const HomePage: React.FC = () => {
-  const { boardsState, getBoards, createBoard, updateBoard, deleteBoard, deleteBoardState } = useContext<any>(ApiBoardsContext);
+  /**
+   * { スピナー, api通信の結果通知の状態管理 }
+   * cssの定義
+   * Boards api import
+   * react-router-dom URLルーティングに使う
+   * ユーザー名をURLパラメータから取得
+   */
   const { progress, status, setStatus } = useContext<any>(FeedbackContext);
   const { useStyles } = useContext<any>(StylesContext);
   const classes = useStyles();
+  const {
+    boardsState,
+    getBoards,
+    createBoard,
+    updateBoard,
+    deleteBoard,
+    deleteBoardState
+  } = useContext<any>(ApiBoardsContext);
   const history = useHistory();
   const { user } = useParams<any>();
 
@@ -59,6 +73,7 @@ export const HomePage: React.FC = () => {
           }}
         />
       </Container>
+
       <Spinner open={progress} />
       <Notice
         open={status.open}
