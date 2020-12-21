9c7aafc114df0ad75d678d75e36f13c4cafd5a6c
sait0-kun <kitsit.39@gmail.com>
2020-12-21 11:46:46 +0900

    [add]#160 TextFromのテスト作成&クリア
    スナップショットテスト

---
+++ work/resources/ts/components/atoms/tests/TextForm.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { TextForm } from '../TextForm';
+
+describe('TextForm', () => {
+  it('スナップショットテスト', () => {
+    const tree = renderer
+      .create(<TextForm />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
