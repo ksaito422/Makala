71a85c6f4c36aac8ab2d01bddf9b33e93221d1d4
sait0-kun <kitsit.39@gmail.com>
2020-12-21 10:02:26 +0900

    [add]#160 DownloadIconのテスト作成&クリア
    ユニットテスト&スナップショットテスト

---
+++ work/resources/ts/components/atoms/tests/DownloadIcon.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
+import { DownloadIcon } from '../DownloadIcon';
+
+describe('DownloadIcon', () => {
+  it('clickイベントの確認', () => {
+    const props = {
+      onClick: jest.fn
+    }
+    // onClickの監視
+    // propsを受け取り mount テスト対象component生成
+    const spyOnClick = jest.spyOn(props, 'onClick');
+    const component = mount(<DownloadIcon {...props} />);
+
+    // clickイベント実行
+    // spyOnClickが実行されたか確認
+    component.find('button').simulate('click');
+    expect(spyOnClick).toHaveBeenCalled();
+  });
+
+  it('スナップショットテスト', () => {
+    const props = {
+      onClick: jest.fn
+    }
+
+    const tree = renderer
+      .create(<DownloadIcon {...props} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
