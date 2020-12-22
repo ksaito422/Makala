a5a21c3507448c073378c07cf87341685bf7d7ec
sait0-kun <kitsit.39@gmail.com>
2020-12-22 11:13:24 +0900

    [add]#166 AccordionAreaのテスト作成&クリア
    スナップショットテスト

---
+++ work/resources/ts/components/molecules/tests/AccordionArea.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { AccordionArea } from '../AccordionArea';
+
+describe('AccordionArea', () => {
+  it('スナップショットテスト', () => {
+    const tree = renderer
+      .create(<AccordionArea />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
