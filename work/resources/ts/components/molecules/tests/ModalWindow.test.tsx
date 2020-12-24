3f8569958aa9e39dbb7bf01f94808eafeead0030
sait0-kun <kitsit.39@gmail.com>
2020-12-24 09:25:23 +0900

    [add]#166 ModalWindowのテスト作成&クリア
    スナップショットテスト

---
+++ work/resources/ts/components/molecules/tests/ModalWindow.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { ModalWindow } from '../ModalWindow';
+import { StylesContextProvider } from '../../../contexts/childContexts/StylesContext';
+
+describe('ModalWindow', () => {
+
+  it('スナップショットテスト', () => {
+    const props = {
+      modalOpen: true,
+      modalOnClose: jest.fn
+    }
+
+    const tree = mount(
+      <StylesContextProvider>
+        <ModalWindow {...props} />
+      </StylesContextProvider>
+      )
+    expect(tree).toMatchSnapshot();
+  });
+});
