ad220f056043c05631578a84785f8ee3323771c2
sait0-kun <kitsit.39@gmail.com>
2020-12-27 13:26:06 +0900

    [change]テスト方法の変更

--- work/resources/ts/components/atoms/tests/CloseIcon.test.tsx
+++ work/resources/ts/components/atoms/tests/CloseIcon.test.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { CloseIcon } from '../CloseIcon';
 
 describe('CloseIcon', () => {
-  it('clickイベントの確認', () => {
-    const props = {
-      onClick: jest.fn,
-    };
-
-    // onClickの監視
-    // propsを受け取り mount テスト対象component生成
-    const spyOnClick = jest.spyOn(props, 'onClick');
-    const component = mount(<CloseIcon {...props} />);
-
-    // clickイベント実行
-    // spyOnClickが実行されたか確認
-    component.find('button').simulate('click');
-    expect(spyOnClick).toHaveBeenCalled();
-  });
-
   it('スナップショットテスト', () => {
     const props = {
       onClick: jest.fn,
@@ -27,5 +11,23 @@ describe('CloseIcon', () => {
 
     const tree = renderer.create(<CloseIcon {...props} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+  it('clickイベントの確認', () => {
+    const props = {
+      onClick: jest.fn,
+    };
+
+    // onClickの監視
+    // propsを受け取りrender テスト対象component生成
+    const spyOnClick = jest.spyOn(props, 'onClick');
+    const component = render(<CloseIcon {...props} />);
+    // screen.debug();
+
+    // <button>を探す
+    // clickイベント実行
+    // spyOnClickが実行されたか確認
+    const button = component.getByRole('button');
+    fireEvent.click(button);
+    expect(spyOnClick).toHaveBeenCalled();
   });
 });
