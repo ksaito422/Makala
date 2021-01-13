f6193c4ff6485fe0c193d569136f684c409279ec
sait0-kun <kitsit.39@gmail.com>
2021-01-13 13:28:49 +0900

    [update]#76 スピナーと通知の実装

--- work/resources/ts/containers/pages/SettingAccountPage.tsx
+++ work/resources/ts/containers/pages/SettingAccountPage.tsx
@@ -1,17 +1,22 @@
 import React, { useContext } from 'react';
 import { Container, CssBaseline, Grid } from '@material-ui/core';
+import { Spinner } from '../../components/molecules/Spinner';
+import { Notice } from '../../components/molecules/Notice';
 import { Header } from '../organisms/Header';
 import { SettingList } from '../organisms/SettingList';
 import { Account } from '../organisms/Account';
 import { AuthContext } from '../../contexts/childContexts/AuthContext';
 import { ApiAccountContext } from '../../contexts/childContexts/ApiAccountContext';
+import { FeedbackContext } from '../../contexts/childContexts/FeedbackContext';
 import { StylesContext } from '../../contexts/childContexts/StylesContext';
 
 export const SettingAccountPage: React.FC = () => {
   /**
+   * { スピナー, api通信の結果通知の状態管理 }
    * cssの定義
    * アカウント情報の読み込み
    */
+  const { progress, status, setStatus } = useContext<any>(FeedbackContext);
   const { useStyles } = useContext<any>(StylesContext);
   const classes = useStyles();
   const { authUserState } = useContext<any>(AuthContext);
@@ -37,6 +42,16 @@ export const SettingAccountPage: React.FC = () => {
           </Grid>
         </Grid>
       </Container>
+
+      <Spinner open={progress} />
+      <Notice
+        open={status.open}
+        type={status.type}
+        message={status.message}
+        onClose={() => {
+          setStatus({ ...status, open: false });
+        }}
+      />
     </>
   );
 };
