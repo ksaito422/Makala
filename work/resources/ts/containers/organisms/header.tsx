16afc3be94bcdfb959e8acd7ca6dc553fbadf18d
sait0-kun <kitsit.39@gmail.com>
2021-01-15 15:16:09 +0900

    [fix]メニューの表示位置を修正
    共通コンポーネント化したら位置ずれたため直した

--- work/resources/ts/containers/organisms/header.tsx
+++ work/resources/ts/containers/organisms/header.tsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AppBar, Grid, Menu, MenuItem, Toolbar, useMediaQuery } from '@material-ui/core';
 import { Title } from '../../components/atoms/Title';
+import { Button } from '../../components/atoms/Button';
 import { Avatar } from '../../components/atoms/Avatar';
 import { AuthContext } from '../../contexts/childContexts/AuthContext';
 
@@ -31,20 +32,6 @@ export const Header: React.FC = () => {
           {matches ? (
             // PCレイアウト width >= 1101px
             <Grid container spacing={2} alignItems='center'>
-              <Grid item xs={11}>
-                <Title />
-              </Grid>
-              <Grid item xs={1}>
-                {isAuth ? (
-                  <Avatar ariaControls='avatar' ariaHaspopup onClick={handleClick}>
-                    {authUserState.name.slice(0, 1)}
-                  </Avatar>
-                ) : null}
-              </Grid>
-            </Grid>
-          ) : (
-            // タブレット・スマホレイアウト width <= 1100px
-            <Grid container spacing={2} alignItems='center'>
               <Grid item xs={10}>
                 <Title />
               </Grid>
@@ -53,7 +40,41 @@ export const Header: React.FC = () => {
                   <Avatar ariaControls='avatar' ariaHaspopup onClick={handleClick}>
                     {authUserState.name.slice(0, 1)}
                   </Avatar>
-                ) : null}
+                ) : (
+                  <Button
+                    variant='outlined'
+                    size='medium'
+                    onClick={() => {
+                      history.push('/login');
+                    }}
+                  >
+                    ログイン
+                  </Button>
+                )}
+              </Grid>
+            </Grid>
+          ) : (
+            // タブレット・スマホレイアウト width <= 1100px
+            <Grid container spacing={2} alignItems='center'>
+              <Grid item xs={9}>
+                <Title />
+              </Grid>
+              <Grid item xs={3}>
+                {isAuth ? (
+                  <Avatar ariaControls='avatar' ariaHaspopup onClick={handleClick}>
+                    {authUserState.name.slice(0, 1)}
+                  </Avatar>
+                ) : (
+                  <Button
+                    variant='outlined'
+                    size='medium'
+                    onClick={() => {
+                      history.push('/login');
+                    }}
+                  >
+                    ログイン
+                  </Button>
+                )}
               </Grid>
             </Grid>
           )}
