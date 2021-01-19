096c380dc73ace4e2a625e9fc71ac6aa7c611293
sait0-kun <kitsit.39@gmail.com>
2021-01-19 16:44:26 +0900

    [add]#217 モーダルのReactMDEを実装

--- work/resources/ts/components/molecules/ModalCard.tsx
+++ work/resources/ts/components/molecules/ModalCard.tsx
@@ -1,26 +1,25 @@
-import React, { useContext } from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState, useContext } from 'react';
+import ReactMde from 'react-mde';
+import 'react-mde/lib/styles/css/react-mde-all.css';
 import { Grid } from '@material-ui/core';
-import { TextForm } from '../atoms/TextForm';
 import { CloseIcon } from '../atoms/CloseIcon';
-import { SubmitIcon } from '../atoms/SubmitIcon';
+import { Button } from '../atoms/Button';
 import { StylesContext } from '../../contexts/childContexts/StylesContext';
 
 type Props = {
-  defaultValueTitle: string;
-  defaultValueContent: string;
-  postOnSubmit: (data: { [x: string]: any }) => void;
+  defaultValue: string;
+  postOnSubmit: (value: string) => void;
   modalOnClose: () => void;
 };
 
 export const ModalCard: React.FC<Props> = (props) => {
   /**
    * cssの定義
-   * API import of react-hook-form
+   * ReactMdeの表示テキストと編集メソッド
    */
   const { useStyles } = useContext<any>(StylesContext);
   const classes = useStyles();
-  const { register, handleSubmit, errors } = useForm();
+  const [value, setValue] = useState<string>(props.defaultValue);
 
   // モーダルに表示する内容の定義
   return (
@@ -29,46 +28,20 @@ export const ModalCard: React.FC<Props> = (props) => {
         <Grid item xs={12} className={classes.rightPlacement}>
           <CloseIcon onClick={props.modalOnClose} />
         </Grid>
-        <form
-          className={classes.form_board}
-          onSubmit={handleSubmit((data) => {
-            props.postOnSubmit(data);
-          })}
-        >
-          <Grid container spacing={4}>
-            <Grid item xs={12}>
-              <TextForm
-                name='card_name'
-                multiline
-                fullWidth
-                rowsMax={1}
-                defaultValue={props.defaultValueTitle}
-                inputRef={register({
-                  required: 'カード名を入力して下さい',
-                })}
-                error={Boolean(errors.card_name)}
-                helperText={errors.card_name && errors.card_name.message}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextForm
-                name='card_content'
-                multiline
-                fullWidth
-                rows={8}
-                defaultValue={props.defaultValueContent}
-                inputRef={register({
-                  required: 'カードの内容を入力して下さい',
-                })}
-                error={Boolean(errors.card_content)}
-                helperText={errors.card_content && errors.card_content.message}
-              />
-            </Grid>
-            <Grid item xs={12} className={classes.centerPlacement}>
-              <SubmitIcon />
-            </Grid>
+        <Grid container spacing={4}>
+          <Grid item xs={12}>
+            <ReactMde value={value} onChange={setValue} />
           </Grid>
-        </form>
+          <Grid item xs={12} className={classes.centerPlacement}>
+            <Button
+              onClick={() => {
+                props.postOnSubmit(value);
+              }}
+            >
+              SAVE
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
