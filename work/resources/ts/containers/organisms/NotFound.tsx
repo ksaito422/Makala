eaa4e2830c9e499a249497cb893b8eeb5c5e60ea
sait0-kun <kitsit.39@gmail.com>
2021-01-20 13:37:11 +0900

    [add]404 Not Foundページの追加
    存在しないURLにアクセスしたら表示するため

---
+++ work/resources/ts/containers/organisms/NotFound.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Paper, Typography } from '@material-ui/core';
+import { StylesContext } from '../../contexts/childContexts/StylesContext';
+
+export const NotFound: React.FC = () => {
+  /**
+   * cssの定義
+   */
+  const { useStyles } = useContext<any>(StylesContext);
+  const classes = useStyles();
+
+  return (
+    <>
+      <Container maxWidth='md'>
+        <Paper elevation={2} className={classes.paper}>
+          <Typography variant='h3' color='textPrimary'>
+            Not Found
+          </Typography>
+          <Typography variant='h5' color='textSecondary'>
+            お探しのページは見つかりませんでした。
+          </Typography>
+          <Link to='/'>TOPへ</Link>
+        </Paper>
+      </Container>
+    </>
+  );
+};
