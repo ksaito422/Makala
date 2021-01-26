0290e44c624ff25e6286da17a40a1ce3cd1be604
sait0-kun <kitsit.39@gmail.com>
2021-01-26 15:29:40 +0900

    [add]リレーション追加
    card:boardの多:一関係

--- work/app/Model/Card.php
+++ work/app/Model/Card.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Board;
 
 class Card extends Model
 {
-    //
+    public function board()
+    {
+        return $this->belongsTo(Board::class);
+    }
 }
