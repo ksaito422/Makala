c75c27fae0f6b2a6ae8430e8bfd5a164e4a72f91
sait0-kun <kitsit.39@gmail.com>
2021-01-30 11:32:11 +0900

    [clean]可読性向上のためコメント追加

--- work/resources/ts/containers/organisms/SettingList.tsx
+++ work/resources/ts/containers/organisms/SettingList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Paper } from '@material-ui/core';
 import { SettingListItem } from '../../components/molecules/SettingListItem';
 
+// 設定メニューのコンテンツ
 const texts = ['アカウント'];
 
 export const SettingList: React.FC = () => {
